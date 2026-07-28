0d571341e1a927a12a7eb7962ad39c38938ee50a
LAPTOP-G43MA5MO\ekarni <eyalk5@gmail.com>
2026-07-28 03:42:43 +0300

    add live Lean action smoke test

---
+++ Simple.lean
@@ -0,0 +1,12 @@
+namespace Simple
+
+theorem add_zero (n : Nat) : n + 0 = n := by
+  induction n with
+  | zero => rfl
+  | succ n _ =>
+      simp
+
+theorem add_one (n : Nat) : n + 1 = Nat.succ n := by
+  simp
+
+end Simple
